5ba45d72aaa879c7aa9d7d5d5eaf97af49b91452
SonbanhThai <nguyentienlen@gmail.com>
2021-09-25 03:23:07 +0700

    Animation

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+var box_lists = $$('.hidden');
+// console.log(box_lists);
+
+
+
+//code slider
+function slide(){
+    var index = 0;
+    setInterval(function(){
+        for(var box of box_lists){
+            box.classList.remove('active');
+        }
+        // console.log(box_lists[index]);
+        box_lists[index].classList.toggle('active');
+        index++;
+        if(index >= box_lists.length){
+            index = 0;
+        }
+    }, 5000);
+}
+    
+slide()
+
+
+
